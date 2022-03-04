0fe26808e6eb06be263ad8691ecf0eb8bfe5c4a1
Asif Taj <47206155+axiftaj@users.noreply.github.com>
2022-03-04 23:24:21 +0500

    init
    init

--- bazar_updated/android/app/src/main/kotlin/com/example/bazar_updated/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bazar_updated
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- bazar_updated/lib/view/utilz/constants.dart
+++
@@ -1,6 +0,0 @@
-const aBook = 'Avenir Book' ;
-const aHeavy = 'Avenir Heavy' ;
-const aRoman = 'Avenir Roman' ;
-const aLight = 'Avenir Light' ;
-const aBlack = 'Avenir Black' ;
-const aMedium = 'Avenir Medium' ;
