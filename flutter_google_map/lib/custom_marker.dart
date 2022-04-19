50e95c9b1a011c1e07d540b186c4ca38f5f793de
Asif Taj <47206155+axiftaj@users.noreply.github.com>
2022-04-19 22:15:24 +0500

    add list of custom marker

--- flutter_google_map/lib/custom_marker.dart
+++ flutter_google_map/lib/custom_marker.dart
@@ -20,7 +20,9 @@ class _CustomMarkerScreenState extends State<CustomMarkerScreen> {
 
   final Completer<GoogleMapController> _controller = Completer();
 
-   Uint8List? markerImage;
+  List<String> images = [ 'images/car.png' , 'images/marker.png' ,];
+
+  Uint8List? markerImage;
   final List<Marker> _markers =  <Marker>[];
 
   static const CameraPosition _kGooglePlex =  CameraPosition(
@@ -32,7 +34,9 @@ class _CustomMarkerScreenState extends State<CustomMarkerScreen> {
     ByteData data = await rootBundle.load(path);
     ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(), targetWidth: width);
     ui.FrameInfo fi = await codec.getNextFrame();
+
     return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!.buffer.asUint8List();
+
   }
 
 
@@ -46,21 +50,35 @@ class _CustomMarkerScreenState extends State<CustomMarkerScreen> {
   }
 
   loadData()async{
-    final Uint8List markerIcon = await getBytesFromAsset('images/marker.png', 100);
 
-    List<Marker> list =  [
-      Marker(
-          markerId: MarkerId('1'),
-          position: LatLng(33.6844, 73.0479),
-          icon: BitmapDescriptor.fromBytes(markerIcon),
-          infoWindow: InfoWindow(
-              title: 'The title of the marker'
-          )
-      ),
+    for(int i = 0 ; i < images.length ; i++){
+      print('name'+images[i].toString());
+      final Uint8List markerIcon = await getBytesFromAsset(images[i].toString(), 100);
 
-    ];
+      if(i == 1 ){
+        _markers.add(Marker(
+                markerId: MarkerId('2'),
+                position: LatLng(33.6992,  72.9744),
+                icon: BitmapDescriptor.fromBytes(markerIcon),
+                infoWindow: InfoWindow(
+                    title: 'The title of the marker'
+                )
+            ));
+      }else {
+        _markers.add( Marker(
+            markerId: MarkerId(i.toString()),
+            position: LatLng(33.6844, 73.0479),
+            icon: BitmapDescriptor.fromBytes(markerIcon),
+            infoWindow: InfoWindow(
+                title: 'The title of the marker'
+            )));
+      }
 
-    _markers.addAll(list);
+      setState(() {
+
+      });
+    }
+
 
 
   }
