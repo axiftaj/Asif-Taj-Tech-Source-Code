cd5e7b402e0838752cdec42a9886cec8883fc111
Asif Taj <47206155+axiftaj@users.noreply.github.com>
2022-04-22 02:22:35 +0500

    custom info window added

--- flutter_google_map/lib/main.dart
+++ flutter_google_map/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:googlemap/convert_latlang_to_address.dart';
 import 'package:googlemap/current_location.dart';
 import 'package:googlemap/custom_marker.dart';
+import 'package:googlemap/custom_marker_info_window.dart';
 import 'package:googlemap/google_search_places_api.dart';
 import 'package:googlemap/home_screen.dart';
 
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  CustomMarkerScreen(),
+      home:  CustomMarkerInfoWindowScreen(),
     );
   }
 }
